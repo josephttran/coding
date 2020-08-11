710a44fca747e74fb0ba7e520e50abcfac4d136a
josephttran <42635012+josephttran@users.noreply.github.com>
2020-08-11 11:55:39 -0700

    Add code for Two Sum

---
+++ Csharp/Code.Tests/ArrayTopicTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace Code.Tests
+{
+    [TestFixture]
+    class ArrayTopicTests
+    {
+        private ArrayTopic _ArrayTopic;
+
+        [SetUp]
+        public void Setup()
+        {
+            _ArrayTopic = new ArrayTopic();
+        }
+
+        [Test]
+        public void TwoSum_ArrayOfIntegerAndTargetValueInput_ReturnTwoIndicesWhereValueSumEqualTargetValue()
+        {
+            // Arrange
+            int[] nums = { 0, 4, 5, 2, 13, 22 };
+            int target = 15;
+            int[] expected = { 3, 4 };
+            
+            // Act
+            int[] result = _ArrayTopic.TwoSum(nums, target);
+
+            // Assert
+            Assert.AreEqual(result, expected);
+        }
+    }
+}
---
+++ Csharp/Code/ArrayTopic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Code
+{
+    public class ArrayTopic
+    {
+		/* Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+		 * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+		 */
+		public int[] TwoSum(int[] nums, int target)
+		{
+			Dictionary<int, int> dictionary = new Dictionary<int, int>();
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				int complement = target - nums[i];
+
+				if (dictionary.TryGetValue(complement, out int value))
+                {
+					return new int[] { value, i };
+                }
+
+				if (dictionary.TryGetValue(nums[i], out _))
+                {
+					continue;
+                }
+
+				dictionary.Add(nums[i], i);
+			}
+
+			throw new ArgumentException("No two sum solution");
+		}
+	}
+}
