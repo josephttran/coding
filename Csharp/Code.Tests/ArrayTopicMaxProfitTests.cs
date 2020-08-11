89e4f940c81f0b9b19d00453388c353177db9cd8
josephttran <42635012+josephttran@users.noreply.github.com>
2020-08-11 14:32:04 -0700

    Add code for Best Time to Buy and Sell Stock

---
+++ Csharp/Code.Tests/ArrayTopicMaxProfitTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+
+namespace Code.Tests
+{
+    [TestFixture]
+    class ArrayTopicMaxProfitTests
+    {
+        private ArrayTopic _ArrayTopic;
+
+        [SetUp]
+        public void Setup()
+        {
+            _ArrayTopic = new ArrayTopic();
+        }
+
+        [Test]
+        public void MaxProfit_EmptyPrices_ReturnZero()
+        {
+            int[] stockPrices = { };
+
+            int profit = _ArrayTopic.MaxProfit(stockPrices);
+
+            Assert.AreEqual(profit, 0);
+        }
+
+        [Test]
+        public void MaxProfit_BuyPriceLargerThanSellPrices_ReturnZero()
+        {
+            int[] stockPrices = { 7, 6, 4, 3, 1 };
+
+            int profit = _ArrayTopic.MaxProfit(stockPrices);
+
+            Assert.AreEqual(profit, 0);
+        }
+
+        [TestCase(new int[] { 7, 1, 5, 3, 6, 4 }, 5)]
+        [TestCase(new int[] { 2, 4, 1 }, 2)]
+        [TestCase(new int[] { 3, 2, 6, 5, 0, 3 }, 4)]
+        public void MaxProfit_HasHigherSellPrices_ReturnMaxProfit(int[] stockPrices, int expected)
+        {
+            int profit = _ArrayTopic.MaxProfit(stockPrices);
+
+            Assert.AreEqual(profit, expected);
+        }
+    }
+}
--- Csharp/Code/ArrayTopic.cs
+++ Csharp/Code/ArrayTopic.cs
@@ -31,5 +31,43 @@ namespace Code
 
 			throw new ArgumentException("No two sum solution");
 		}
+
+		/* Say you have an array for which the ith element is the price of a given stock on day i.
+		 * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+		 * Note that you cannot sell a stock before you buy one.
+		 */
+		public int MaxProfit(int[] prices)
+		{
+			if (prices.Length == 0)
+            {
+				return 0;
+            }
+
+			int minPrice = prices[0];
+			int sellPrice = prices[0];
+			int maxProfit = 0;
+			
+			for (int i = 0; i < prices.Length; ++i)
+            {
+				if (minPrice > prices[i] && i + 1 != prices.Length)
+                {
+					minPrice = prices[i];
+					sellPrice = prices[i];
+                }
+
+				if (sellPrice < prices[i])
+				{
+					sellPrice = prices[i];
+					maxProfit = Math.Max(maxProfit, sellPrice - minPrice);
+				}
+			}
+
+			if (maxProfit > 0)
+			{
+				return maxProfit;
+			}
+
+			return 0;
+		}
 	}
 }
