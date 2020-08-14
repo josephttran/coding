2d6a7784f32aa9a8ef98724b1eacc64a44ea81f7
josephttran <42635012+josephttran@users.noreply.github.com>
2020-08-14 16:52:16 -0700

    Add divide and conquer code for Maximum Subarray

--- Csharp/Code.Tests/ArrayTopicMaxSubArray.cs
+++ Csharp/Code.Tests/ArrayTopicMaxSubArray.cs
@@ -14,12 +14,33 @@ namespace Code.Tests
         }
 
         [Test]
-        public void MaxSubArray_ReturnM()
+        public void MaxSubArray_InputIntegerArray_ReturnMaxSum()
         {
             int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
             int expected = 6;
 
             int actual = _ArrayTopic.MaxSubArray(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void MaxSubArrayDAC_InputIntegerArray_ReturnMaxSum()
+        {
+            int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            int expected = 6;
+
+            int actual = _ArrayTopic.MaxSubArrayDAC(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(new int[] { 4 }, 4)]
+        [TestCase(new int[] { -2, 1, -3, 4, 10 }, 14)]
+        [TestCase(new int[] { 4, 1, -3, 4, -5, 10, -2 }, 11)]
+        public void MaxSubArrayDAC_InputIntegers_ReturnMaxSum(int[] nums, int expected)
+        {
+            int actual = _ArrayTopic.MaxSubArrayDAC(nums);
 
             Assert.AreEqual(expected, actual);
         }
--- Csharp/Code/ArrayTopic.cs
+++ Csharp/Code/ArrayTopic.cs
@@ -91,6 +91,66 @@ namespace Code
             }
 		}
 
+		public int MaxSubArrayDAC(int[] nums)
+		{
+			if (nums.Length == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				int MaxSumSubarray(int[] arr, int low, int high)
+                {
+					if (low == high)
+                    {
+						return arr[high];
+                    }
+
+					int middle = (high + low) / 2;
+
+					int leftSumSubarray = MaxSumSubarray(nums, low, middle);
+					int rightSumSubarray = MaxSumSubarray(nums, middle + 1, high);
+					int crossingSumSubarray = MaxSumSubarrayCrossing(nums, low, middle, high);
+
+					return Math.Max(Math.Max(leftSumSubarray, rightSumSubarray), crossingSumSubarray);
+				}
+
+				int MaxSumSubarrayCrossing(int[] arr, int low, int mid, int high)
+                {
+					int leftSum = Int32.MinValue;
+					int sum = 0;
+
+					for (int i = mid; i >= low; --i)
+                    {
+						sum += arr[i];
+						if (leftSum < sum)
+                        {
+							leftSum = sum;
+                        }
+                    }
+
+					int rightSum = Int32.MinValue;
+					sum = 0;
+
+					for (int i = mid + 1; i <= high; ++i)
+					{
+						sum += arr[i];
+						if (rightSum < sum)
+						{
+							rightSum = sum;
+						}
+					}
+
+					return (leftSum + rightSum);
+                }
+
+				int maxSum = MaxSumSubarray(nums, 0, nums.Length - 1);
+
+				return maxSum;
+			}
+		}
+
+
 		/* Say you have an array for which the ith element is the price of a given stock on day i.
 		 * If you were only permitted to complete at most one transaction 
 		 * (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
