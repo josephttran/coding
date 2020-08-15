de94e6b31bcf9f6360c16a5fb8cac34482346945
josephttran <42635012+josephttran@users.noreply.github.com>
2020-08-15 04:20:08 -0700

    Add code for Maximum Product Subarray

---
+++ Csharp/Code.Tests/ArrayTopicMaxProductSubarray.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+namespace Code.Tests
+{
+    [TestFixture]
+    class ArrayTopicMaxProductSubarray
+    {
+        private ArrayTopic _ArrayTopic;
+
+        [SetUp]
+        public void Setup()
+        {
+            _ArrayTopic = new ArrayTopic();
+        }
+
+        [Test]
+        public void MaxProductSubarray_ArrayOneNumber_ReturnNumber()
+        {
+            int[] nums = { 4 };
+            int expected = 4;
+
+            int actual = _ArrayTopic.MaxProductSubarray(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase(new int[] { -1, -2, -1, 2, 2, 1, 1 }, 8)]
+        [TestCase(new int[] { 2, -5, -2, -4, 3 }, 24)]
+        [TestCase(new int[] { 6, 3, -10, 0, 2 }, 18)]
+        [TestCase(new int[] { 2, 3 }, 6)]
+        [TestCase(new int[] { -3, 0, 1, -2 }, 1)]
+        [TestCase(new int[] { -5, 0, 2 }, 2)]
+        [TestCase(new int[] { -3, 0, -1, -4, 0, -1 }, 4)]
+        [TestCase(new int[] { -3, -1, -1 }, 3)]
+        [TestCase(new int[] { 3, -1, 4 }, 4)]
+        [TestCase(new int[] { 0, 0 }, 0)]
+        [TestCase(new int[] { 0, 0, 1 }, 1)]
+        [TestCase(new int[] { 0, 0, 2, 0, 0, 1 }, 2)]
+        [TestCase(new int[] { 0 }, 0)]
+        [TestCase(new int[] { 0, 2 }, 2)]
+        [TestCase(new int[] { 2, 3, 4 }, 24)]
+        [TestCase(new int[] { 2, 3, -2, 4 }, 6)]
+        [TestCase(new int[] { -2, 0, -1 }, 0)]
+        [TestCase(new int[] { 4, 0, -3, 5, -2, 0, 4, 5}, 30)]
+        [TestCase(new int[] { 0, -3, 5, -2, 0, 4, 5 }, 30)]
+        public void MaxProductSubarray_ArrayIntegers_ReturnMaxProduct(int[] nums, int expected)
+        {
+            int actual = _ArrayTopic.MaxProductSubarray(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- Csharp/Code/ArrayTopic.cs
+++ Csharp/Code/ArrayTopic.cs
@@ -190,6 +190,189 @@ namespace Code
 			return 0;
 		}
 
+		/* Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+		 */
+		public int MaxProductSubarray(int[] nums)
+		{
+			if (nums.Length == 1)
+            {
+				return nums[0];
+            }
+	
+			// Create subarray
+			List<List<int>> listOfSubarray = new List<List<int>>();
+			List<int> subarray = new List<int>();
+
+			for (int i = 0; i < nums.Length; ++i)
+            {
+				if (nums[i] != 0)
+                {
+					subarray.Add(nums[i]);
+                }
+				else
+                {
+					if (subarray.Count > 0)
+                    {
+						listOfSubarray.Add(subarray);
+						subarray = new List<int>();
+                    }
+                }
+            }
+
+			if (subarray.Count > 0)
+			{
+				listOfSubarray.Add(subarray);
+			}
+
+			if (listOfSubarray.Count == 0)
+            {
+				return 0;
+            }
+
+			// Find max product of subarray
+			if (listOfSubarray.Any(arr => arr.Any(num => num > 0)))
+			{
+				int maxProductSub = listOfSubarray.Select(subarr =>
+				{		
+					int arrNegativeCount = subarr.Count(a => a < 0);
+
+					if (arrNegativeCount % 2 == 0)
+					{
+						return subarr.Aggregate((acc, num) => acc * num);
+					}
+					else
+					{
+						if (subarr.Count < 3)
+						{
+							return subarr.Max();
+						}
+
+						if (arrNegativeCount == 1)
+						{
+							int indexOfNegative = subarr.FindIndex((arr) => arr < 0);
+
+							if (indexOfNegative == 0)
+                            {
+								return subarr.Skip(1).Aggregate((acc, num) => acc * num);
+							}
+							
+							if (indexOfNegative == subarr.Count - 1)
+                            {
+								return subarr.Take(subarr.Count - 1).Aggregate((acc, num) => acc * num);
+							}
+
+							return Math.Max(
+								subarr.Take(indexOfNegative).Aggregate((acc, num) => acc * num),
+								subarr.Skip(indexOfNegative + 1).Aggregate((acc, num) => acc * num)
+							);
+						}
+
+						int negativeCount = arrNegativeCount;
+						// max left
+						int maxProduct = 1;
+						int currentProduct = 1;
+
+						foreach (int num in subarr)
+						{
+							if (num < 0)
+							{
+								if (negativeCount > 1)
+								{
+									negativeCount--;
+								}
+								else
+								{
+									if (maxProduct < currentProduct)
+									{
+										maxProduct = Math.Max(maxProduct, currentProduct);
+										currentProduct = 1;
+										continue;
+									}
+								}
+							}
+
+							currentProduct *= num;
+						}
+
+						// max right
+						subarr.Reverse();
+						negativeCount = arrNegativeCount;
+						arrNegativeCount = 
+						currentProduct = 1;
+						foreach (int num in subarr)
+						{
+							if (num < 0)
+							{
+								if (negativeCount > 1)
+								{
+									negativeCount--;
+								}
+								else
+								{
+									if (maxProduct < currentProduct)
+									{
+										maxProduct = Math.Max(maxProduct, currentProduct);
+										currentProduct = 1;
+										continue;
+									}
+								}
+							}
+
+							currentProduct *= num;
+						}
+
+						return maxProduct;
+					}
+				}).Max();
+
+				return maxProductSub;
+			}
+			else
+			{
+				// Subarray contains no postive number
+				int maxProductSub = listOfSubarray.Select(subarr =>
+				{
+					int arrNegativeCount = subarr.Count(a => a < 0);
+
+					if (subarr.Contains(0))
+                    {
+						int indexOfNegative = subarr.FindIndex((arr) => arr == 0);
+						int leftProduct = subarr.Take(indexOfNegative).Aggregate((acc, num) => acc * num);
+						int rightProduct = subarr.Skip(indexOfNegative + 1).Aggregate((acc, num) => acc * num);
+
+						return Math.Max(0, Math.Max(leftProduct, rightProduct));
+                    }
+					else
+                    {
+						if (arrNegativeCount % 2 == 0)
+						{
+							return subarr.Aggregate((acc, num) => acc * num);
+						}
+						else
+						{
+							// Section
+							// Subarray contains more than 2 negative
+							int[] negativeNumbersIndices = subarr.Select((arr, index) => arr < 0 ? index : -1).Where(i => i != -1).ToArray();
+							int max = 0;
+
+							for (int i = 0; i < negativeNumbersIndices.Length; ++i)
+                            {
+								int indexOfNegative = negativeNumbersIndices[i];
+								int leftProduct = subarr.Take(indexOfNegative + 1).Aggregate((acc, num) => acc * num);
+								int rightProduct = subarr.Skip(indexOfNegative).Aggregate((acc, num) => acc * num);
+
+								max = Math.Max(max, Math.Max(leftProduct, rightProduct));
+                            }
+
+							return max;
+						}
+                    }
+				}).Max();
+
+				return maxProductSub;
+			}
+		}
+
 		/* Given an array of integers, find if the array contains any duplicates.
 		 * Your function should return true if any value appears at least twice in the array, 
 		 * and it should return false if every element is distinct.
