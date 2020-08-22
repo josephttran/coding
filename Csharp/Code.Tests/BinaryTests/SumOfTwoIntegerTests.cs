d0eb2890fa8594165f02a8d5993003b6128aa137
josephttran <42635012+josephttran@users.noreply.github.com>
2020-08-22 14:46:22 -0700

    Calculate sum of two integers without using + and - operator

---
+++ Csharp/Code.Tests/BinaryTests/SumOfTwoIntegerTests.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+
+namespace Code.Tests.BinaryTests
+{
+    [TestFixture]
+    class SumOfTwoIntegerTests
+    {
+        private Binary _Binary;
+
+        [SetUp]
+        public void Setup()
+        {
+            _Binary = new Binary();
+        }
+
+        [Test]
+        public void SumOfTwoInteger_AddOneAndTwo_ReturnThree()
+        {
+            // Arrange
+            int firstNum = 1;
+            int secondNum = 2;
+            int expected = 3;
+
+            // Act
+            int actual = _Binary.SumOfTwoInteger(firstNum, secondNum);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SumOfTwoInteger_AddNegativeTwoAndThree_ReturnOne()
+        {
+            int firstNum = -2;
+            int secondNum = 3;
+            int expected = 1;
+
+            int actual = _Binary.SumOfTwoInteger(firstNum, secondNum);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(0, 0, 0)]
+        [TestCase(12, 0, 12)]
+        [TestCase(8, 5, 13)]
+        [TestCase(12, 5, 17)]
+        [TestCase(12, 1225, 1237)]
+        public void SumOfTwoInteger_AddTwoPositiveNumber_ReturnSum(int firstNum, int secondNum, int expected)
+        {
+            int actual = _Binary.SumOfTwoInteger(firstNum, secondNum);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(-12, -1225, -1237)]
+        public void SumOfTwoInteger_AddTwoNegativeNumber_ReturnSum(int firstNum, int secondNum, int expected)
+        {
+            int actual = _Binary.SumOfTwoInteger(firstNum, secondNum);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(0, -12, -12)]
+        [TestCase(-12, 0, -12)]
+        [TestCase(-12, 125, 113)]
+        [TestCase(125, -12, 113)]
+        public void SumOfTwoInteger_AddANegativeNumber_ReturnSum(int firstNum, int secondNum, int expected)
+        {
+            int actual = _Binary.SumOfTwoInteger(firstNum, secondNum);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Csharp/Code/Binary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code
+{
+    public class Binary
+    {
+        // Calculate the sum of two integers a and b, but you are Not allowed to use the operator + and -.
+        public int SumOfTwoInteger(int a, int b)
+        {
+            string binaryA = Convert.ToString(a, 2).PadLeft(32, '0');
+            string binaryB = Convert.ToString(b, 2).PadLeft(32, '0');
+            int sum;
+            StringBuilder sumBinary = new StringBuilder();
+            char carry = '0';
+
+            for (int i = binaryA.Length - 1; i > -1; --i)
+            {
+                int sumChar = Convert.ToInt32(binaryA[i].ToString()) ^ Convert.ToInt32(binaryB[i].ToString()) ^ Convert.ToInt32(carry.ToString());
+                int carryNum;
+
+                if (binaryA[i] == '0')
+                {
+                    carryNum = Convert.ToInt32(carry.ToString()) & Convert.ToInt32(binaryB[i].ToString()) ^ 0;
+                }
+                else
+                {
+                    carryNum = Convert.ToInt32(carry.ToString()) | Convert.ToInt32(binaryB[i].ToString());
+                }
+
+                if (carryNum == 0)
+                {
+                    carry = '0';
+                }
+                else
+                {
+                    carry = '1';
+                }
+
+                sumBinary.Append(sumChar.ToString());
+            }
+                    
+            char[] sumBinaryArr = sumBinary.ToString().ToCharArray();
+
+            Array.Reverse(sumBinaryArr);
+
+            sum = Convert.ToInt32(new string(sumBinaryArr), 2);
+
+            return sum;
+        }
+    }
+}
