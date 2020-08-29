80d1387747be5ec011379951ab8446c9570821bf
josephttran <42635012+josephttran@users.noreply.github.com>
2020-08-29 03:38:24 -0700

    Add code for Counting Bits

---
+++ Csharp/Code.Tests/BinaryTests/CountBitsTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+namespace Code.Tests.BinaryTests
+{
+    [TestFixture]
+    class CountBitsTests
+    {
+        private Binary _Binary;
+
+        [SetUp]
+        public void Setup()
+        {
+            _Binary = new Binary();
+        }
+
+        [Test]
+        public void CountBits_InputNumber_ReturnArrayWithLengthOfNumberPlusOne()
+        {
+            // Arrange
+            int num = 8;
+            int expectedLength = 9;
+
+            // Act
+            int[] countArr = _Binary.CountBits(num);
+            int length = countArr.Length;
+
+            // Assert
+            Assert.AreEqual(expectedLength, length);
+        }
+
+        [Test]
+        public void CountBits_InputNumberTwo_ReturnArrayZeroOneOne()
+        {
+            // Arrange
+            int num = 2;
+            int[] expected = new int[] { 0, 1, 1 };
+
+            // Act
+            int[] actual = _Binary.CountBits(num);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCase(5, new int[] { 0, 1, 1, 2, 1, 2 })]
+        [TestCase(16, new int[] { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1 })]
+        public void CountBits_InputNumber_ReturnArrayCountOnesBits(int num, int[] expected)
+        {
+            int[] actual = _Binary.CountBits(num);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- Csharp/Code/Binary.cs
+++ Csharp/Code/Binary.cs
@@ -6,6 +6,70 @@ namespace Code
 {
     public class Binary
     {
+        /* Given a non negative integer number num. 
+         * For every numbers i in the range 0 ≤ i ≤ num 
+         * calculate the number of 1's in their binary representation
+         * and return them as an array.
+         * O(n)
+         */
+
+        /*
+         * 0,
+         * 1,
+         * 1, 2,
+         * 1, 2, 2, 3,
+         * 1, 2, 2, 3, 2, 3, 3, 4, 
+         * 1, 2  2  3  2  3  3  4  2 3 3 4 3 4 4 5
+         * 1, 2  2  3  2  3  3  4  2 3 3 4 3 4 4 5 3  ...  4 5 5 6              
+        */
+        public int[] CountBits(int num)
+        {
+            if (num == 0)
+            {
+                return new int[] { 0 };
+            }
+
+            if (num == 1)
+            {
+                return new int[] { 0, 1 };
+            }
+
+            int[] countArray = new int[num + 1];
+            int pow = 1;
+            int index = 2;
+            
+            countArray[0] = 0;
+            countArray[1] = 1;
+
+            while (index <= num)
+            {
+                int prevStart = (int) Math.Pow(2, pow - 1);
+                int low = (int) Math.Pow(2, pow);
+                int high = (int) Math.Pow(2, pow + 1) - 1;
+                int mid = (int) Math.Ceiling((high + low) / 2.0);
+
+                for (int i = low; i < mid && index <= num; ++i)
+                {
+                    countArray[i] = countArray[prevStart];
+                    prevStart++;
+                    index++;
+                }
+
+                prevStart = (int) Math.Pow(2, pow - 1);
+
+                for (int j = mid; j <= high && index <= num; ++j)
+                {
+                    countArray[j] = countArray[prevStart] + 1;
+                    prevStart++;
+                    index++;
+                }
+
+                pow++;
+            }
+
+            return countArray;
+        }
+
         /* Write a function that takes an unsigned integer and return the number of '1' bits it has (also known as the Hamming weight).
          */
         public int HammingWeight(uint n)
