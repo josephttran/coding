879e4f4fc772e676cf0bb985d2bc8e388b89b1b7
josephttran <42635012+josephttran@users.noreply.github.com>
2020-09-01 03:57:23 -0700

    Add code for Climbing Stairs

---
+++ Csharp/Code.Tests/DynamicProgrammingTests/ClimbStairsTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace Code.Tests.DynamicProgrammingTests
+{
+    [TestFixture]
+    class ClimbStairsTests
+    {
+        private DynamicProgramming _dynamicProgramming;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dynamicProgramming = new DynamicProgramming();
+        }
+
+        [Test]
+        public void ClimbStairs_WhenInputOne_ReturnOne()
+        {
+            // Arrange
+            int num = 1;
+            int expected = 1;
+
+            // Act
+            int result = _dynamicProgramming.ClimbStairs(num);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(2, 2)]
+        [TestCase(3, 3)]
+        [TestCase(4, 5)]
+        [TestCase(5, 8)]
+        [TestCase(6, 13)]
+        public void ClimbStairs_WhenNumGreaterThanZeroAndLessThanFortySix_ReturnNumberOfWay(int num, int expected)
+        {
+            int result = _dynamicProgramming.ClimbStairs(num);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test, Timeout(5000)]
+        public void ClimbStairs_WhenInputLargeNumber_ReturnNotTakeLong()
+        {
+            // Arrange
+            int num = 44;
+            int expected = 1134903170;
+
+            // Act
+            int result = _dynamicProgramming.ClimbStairs(num);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Csharp/Code/DynamicProgramming.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code
+{
+    public class DynamicProgramming
+    {
+        /* You are climbing a stair case. It takes n steps to reach to the top.
+         * Each time you can either climb 1 or 2 steps. 
+         * In how many distinct ways can you climb to the top?
+         * 1 <= n <= 45
+         */
+        public int ClimbStairs(int n)
+        {
+            if ( n < 3)
+            {
+                return n;
+            }
+
+            int prevPrev = 1; 
+            int prev = 2;
+            int number = prev + prevPrev;
+            int nBegin = 3;
+
+            while (nBegin != n)
+            {
+                prevPrev = prev;
+                prev = number;
+                number = prev + prevPrev;
+                nBegin++;
+            }
+
+            return number;
+        }
+    }
+}
