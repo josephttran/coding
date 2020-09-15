3f22b16505f14748bfa97f191ea7da90a460b85a
josephttran <42635012+josephttran@users.noreply.github.com>
2020-09-15 14:47:10 -0700

    Add code to get max sum with no adjacent elements

---
+++ Csharp/Code.Tests/DynamicProgrammingTests/HouseRobberTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace Code.Tests.DynamicProgrammingTests
+{
+    [TestFixture]
+    class HouseRobberTests
+    {
+        private DynamicProgramming _dynamicProgramming;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _dynamicProgramming = new DynamicProgramming();
+        }
+
+        [Test]
+        public void HouseRobber_WhenSingleElement_ShouldReturnElement()
+        {
+            int[] nums = new int[] { 4 };
+            int expected = 4;
+
+            int result = _dynamicProgramming.HouseRobber(nums);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void HouseRobber_ShouldBeMaxAmount_WhenSumNonAdjacent()
+        {
+            int[] nums = new int[] { 1, 2, 3, 1 };
+            int expected = 4;
+
+            int result = _dynamicProgramming.HouseRobber(nums);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void HouseRobber_WhenSumNonAdjacent_ReturnMaxAmount()
+        {
+            int[] nums = new int[] { 2, 7, 9, 3, 1 };
+            int expected = 12;
+
+            int result = _dynamicProgramming.HouseRobber(nums);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
--- Csharp/Code/DynamicProgramming.cs
+++ Csharp/Code/DynamicProgramming.cs
@@ -95,6 +95,44 @@ namespace Code
             return dp[target];
         }
 
+        /* You are a professional robber planning to rob houses along a street. 
+         * Each house has a certain amount of money stashed, 
+         * the only constraint stopping you from robbing each of them is that adjacent houses have security system connected 
+         * and it will automatically contact the police if two adjacent houses were broken into on the same night.
+         * 
+         * Given a list of non-negative integers representing the amount of money of each house, 
+         * determine the maximum amount of money you can rob tonight without alerting the police.
+         */
+        public int HouseRobber(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+
+            if (nums.Length == 2)
+            {
+                return Math.Max(nums[0], nums[1]);
+            }
+
+            int[] dp = new int[nums.Length];
+
+            dp[0] = nums[0];
+            dp[1] = Math.Max(nums[0], nums[1]);
+
+            for (int i = 2; i < dp.Length; ++i)
+            {
+                dp[i] = Math.Max(nums[i] + dp[i - 2], dp[i - 1]);
+            }
+
+            return dp[dp.Length - 1];
+        }
+
         /* Given two strings text1 and text2, return the length of their longest common subsequence.
          * A subsequence of a string is a new string generated from the original string with 
          * some characters(can be none) deleted without changing the relative order of the remaining characters. 
