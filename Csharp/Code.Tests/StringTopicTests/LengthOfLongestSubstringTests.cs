3a3b701841faca865db511912b1c5c3c4d1d6785
josephttran <42635012+josephttran@users.noreply.github.com>
2020-10-04 14:36:13 -0700

    Add code for Longest Substring Without Repeating Characters

---
+++ Csharp/Code.Tests/StringTopicTests/LengthOfLongestSubstringTests.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+
+namespace Code.Tests.StringTopicTests
+{
+    [TestFixture]
+    class LengthOfLongestSubstringTests
+    {
+        private StringTopic _stringTopic;
+
+        [SetUp]
+        public void Setup()
+        {
+            _stringTopic = new StringTopic();
+        }
+
+        [Test]
+        public void LengthOfLongestSubstring_WhenEmpty_ReturnZero()
+        {
+            string s = "";
+            int expected = 0;
+
+            int result = _stringTopic.LengthOfLongestSubstring(s);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void LengthOfLongestSubstring_WhenOnlySpace_ReturnOne()
+        {
+            string s = " ";
+            int expected = 1;
+
+            int result = _stringTopic.LengthOfLongestSubstring(s);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("abcabcbb", 3)]
+        [TestCase("bbbbb", 1)]
+        [TestCase("pwwkew", 3)]
+        public void LengthOfLongestSubstring_ShouldReturnLongestSubstringWithoutRepeatCharacter(string s, int expected)
+        {
+            int result = _stringTopic.LengthOfLongestSubstring(s);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Csharp/Code/StringTopic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code
+{
+    public class StringTopic
+    {
+        /* Given a string s, find the length of the longest substring without repeating characters.
+         */
+        public int LengthOfLongestSubstring(string s)
+        {
+            Queue<char> queue = new Queue<char>();
+            int longest = 0;
+
+            for (int i = 0; i < s.Length; ++i)
+            {
+                if (queue.Contains(s[i]))
+                {
+                    while (queue.Contains(s[i]))
+                    {
+                        queue.Dequeue();
+                    }
+                    
+                }
+
+                queue.Enqueue(s[i]);
+                longest = Math.Max(longest, queue.Count);
+            }
+
+            return longest;
+        }
+    }
+}
