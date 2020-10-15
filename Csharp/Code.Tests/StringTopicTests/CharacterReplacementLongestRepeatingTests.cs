5ff002a9d76ab23c3c4b7598397ae18885ff14d9
josephttran <42635012+josephttran@users.noreply.github.com>
2020-10-14 23:24:13 -0700

    Add code for Longest Repeating Character Replacement

---
+++ Csharp/Code.Tests/StringTopicTests/CharacterReplacementLongestRepeatingTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace Code.Tests.StringTopicTests
+{
+    class CharacterReplacementLongestRepeatingTests
+    {
+        private StringTopic _stringTopic;
+
+        [SetUp]
+        public void Setup()
+        {
+            _stringTopic = new StringTopic();
+        }
+
+        [Test]
+        public void CharacterReplacementLongestRepeating_WhenEmpty_ReturnZero()
+        {
+            string s = "";
+            int expected = 0;
+
+            int result = _stringTopic.CharacterReplacementLongestRepeating(s, 0);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("ABAB", 2, 4)]
+        [TestCase("AABABBA", 1, 4)]
+        public void CharacterReplacementLongestRepeating_WhenReplaceCharacters_ReturnLongestRepeatSubstring(string s, int numReplacement, int expected)
+        {
+            int result = _stringTopic.CharacterReplacementLongestRepeating(s, numReplacement);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
--- Csharp/Code/StringTopic.cs
+++ Csharp/Code/StringTopic.cs
@@ -6,6 +6,39 @@ namespace Code
 {
     public class StringTopic
     {
+        /* Given a string s that consists of only uppercase English letters, you can perform at most k operations on that string.
+         * In one operation, you can choose any character of the string and change it to any other uppercase English character.
+         * Find the length of the longest sub-string containing all repeating letters you can get after performing the above operations.
+         */
+        public int CharacterReplacementLongestRepeating(string s, int k)
+        {
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+            int longest = 0;
+            int left = 0;
+
+            for (int right = 0; right < s.Length; ++right)
+            {
+                if (!dictionary.ContainsKey(s[right]))
+                {
+                    dictionary[s[right]] = 1;
+                }
+                else
+                {
+                    dictionary[s[right]] += 1;
+                }
+
+                longest = Math.Max(longest, dictionary[s[right]]);
+
+                if (right - left + 1 > k + longest)
+                {
+                    dictionary[s[left]] -= 1;
+                    left++;
+                }
+            }
+
+            return s.Length - left;
+        }
+
         /* Given a string s, find the length of the longest substring without repeating characters.
          */
         public int LengthOfLongestSubstring(string s)
