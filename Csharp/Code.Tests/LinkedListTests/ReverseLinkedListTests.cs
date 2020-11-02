1edf84635501459777d191ce8056da43c66444e7
josephttran <42635012+josephttran@users.noreply.github.com>
2020-11-02 00:06:52 -0800

    Add code for Reverse Linked List

---
+++ Csharp/Code.Tests/LinkedListTests/ReverseLinkedListTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using Code.Models;
+
+namespace Code.Tests.LinkedListTests
+{
+    [TestFixture]
+    class ReverseLinkedListTests
+    {
+        private LinkedList _linkedList;
+
+        [SetUp]
+        public void Setup()
+        {
+            _linkedList = new LinkedList();
+        }
+
+        [Test]
+        public void ReverseLinkedList_WhenNull()
+        {
+            // Arrange
+            ListNode head = null;
+
+            // Act
+            ListNode result = _linkedList.ReverseLinkedList(head);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void ReverseLinkedList_WhenSingle()
+        {
+            // Arrange
+            ListNode head = new ListNode(4);
+
+            // Act
+            ListNode result = _linkedList.ReverseLinkedList(head);
+
+            // Assert
+            Assert.AreSame(head, result);
+        }
+
+        [Test]
+        public void ReverseLinkedList_ShouldReturnReverseList()
+        {
+            // Arrange
+            int startVal = 1;
+            int endVal = 5;
+            ListNode head = new ListNode(startVal);
+
+            for (int i = startVal + 1; i <= endVal; ++i)
+            {
+                ListNode current = head;
+
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+
+                current.next = new ListNode(i);
+            }
+
+            // Act
+            ListNode result = _linkedList.ReverseLinkedList(head);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.IsNotNull(result);
+
+                int val = endVal;
+
+                while (result.next != null)
+                {
+                    Assert.AreEqual(val, result.val);
+                    result = result.next;
+                    val--;
+                }
+
+                Assert.AreEqual(startVal, result.val);
+            });
+        }   
+    }
+}
---
+++ Csharp/Code/LinkedList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Code.Models;
+
+namespace Code
+{
+    public class LinkedList
+    {
+        public ListNode ReverseLinkedList(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            if (head.next == null)
+            {
+                return head;
+            }
+
+            ListNode current = head;
+            ListNode reverseList = new ListNode(current.val);
+            current = current.next;
+
+            while (current.next != null)
+            {
+                ListNode temp = new ListNode(current.val);
+                temp.next = reverseList;
+                reverseList = temp;
+                current = current.next;
+            }
+
+            ListNode last = new ListNode(current.val);
+            last.next = reverseList;
+
+            return last;
+        }
+    }
+}
