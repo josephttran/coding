51d8aa79d0bca1f2d2536a67562a9fa80e5a170c
josephttran <42635012+josephttran@users.noreply.github.com>
2020-11-22 18:07:35 -0800

    Add code for Set Matrix Zeroes

---
+++ Csharp/Code.Tests/MatrixTests/SetMatrixZeroesTests.cs
@@ -0,0 +1,101 @@
+﻿using NUnit.Framework;
+
+namespace Code.Tests.MatrixTests
+{
+    public class SetMatrixZeroesTests
+    {
+        Matrix _matrix;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _matrix = new Matrix();
+        }
+
+        [Test]
+        public void SetMatrixZeroes_SingleColumnTest()
+        {
+            int[][] matrix = {
+                new int[] { 1 }, 
+                new int[] { 0 },
+                new int[] { 1 }};
+
+            int[][] expectedMatrix = {
+                new int[] { 0 },
+                new int[] { 0 },
+                new int[] { 0 }};
+
+            _matrix.SetMatrixZeroes(matrix);
+
+            CollectionAssert.AreEqual(expectedMatrix, matrix);
+        }
+
+        [Test]
+        public void SetMatrixZeroes_SingleRowTest()
+        {
+            int[][] matrix = { new int[] { 1, 0, 1 } };
+
+            int[][] expectedMatrix = { new int[] { 0, 0, 0 } };
+
+            _matrix.SetMatrixZeroes(matrix);
+
+            CollectionAssert.AreEqual(expectedMatrix, matrix);
+        }
+
+        [Test]
+        public void SetMatrixZeroes_ShouldSetRowAndColumnToZero_WhenElementIsZero()
+        {
+            int[][] matrix = { 
+                new int[] { 1, 1, 1 },
+                new int[] { 1, 0, 1 },
+                new int[] { 1, 1, 1 } };
+
+            int[][] expectedMatrix = {
+                new int[] { 1, 0, 1 },
+                new int[] { 0, 0, 0 },
+                new int[] { 1, 0, 1 } };
+
+            _matrix.SetMatrixZeroes(matrix);
+
+            CollectionAssert.AreEqual(expectedMatrix, matrix);
+        }
+
+        [Test]
+        public void SetMatrixZeroes_WhenElementIsZero_SetRowAndColumnToZero()
+        {
+            int[][] matrix = {
+                new int[] { 0, 1, 2, 0 },
+                new int[] { 3, 4, 5, 2 },
+                new int[] { 1, 3, 1, 5 } };
+
+            int[][] expectedMatrix = {
+                new int[] { 0, 0, 0, 0 },
+                new int[] { 0, 4, 5, 0 },
+                new int[] { 0, 3, 1, 0 } };
+
+            _matrix.SetMatrixZeroes(matrix);
+
+            CollectionAssert.AreEqual(expectedMatrix, matrix);
+        }
+
+        [Test]
+        public void SetMatrixZeroes_WhenElementIsZero_SetRowAndColumnToZero_Test()
+        {
+            int[][] matrix = {
+                new int[] { 1, 2, 3, 4 },
+                new int[] { 5, 0, 7, 8 },
+                new int[] { 0, 10, 11, 12 },
+                new int[] { 13, 14, 15, 0 } };
+
+            int[][] expectedMatrix = {
+                new int[] { 0, 0, 3, 0 },
+                new int[] { 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0 },
+                new int[] { 0, 0, 0, 0 } };
+
+            _matrix.SetMatrixZeroes(matrix);
+
+            CollectionAssert.AreEqual(expectedMatrix, matrix);
+        }
+    }
+}
---
+++ Csharp/Code/Matrix.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Code
+{
+    public class Matrix
+    {
+
+        /* Given an m x n matrix. If an element is 0, set its entire row and column to 0.
+         */
+        public void SetMatrixZeroes(int[][] matrix)
+        {
+            bool zeroRow = Array.IndexOf(matrix[0], 0) != -1;
+            bool zeroColumn = matrix.Any(row => row[0] == 0);
+
+            for (int i = 0; i < matrix.Length; ++i)
+            {
+                for (int j = 0; j < matrix[i].Length; ++j)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
+                }
+            }
+
+            for (int i = 1; i < matrix.Length; ++i)
+            {
+                if (matrix[i][0] == 0)
+                {
+                    for (int j = 1; j < matrix[i].Length; ++j)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+
+            for (int j = 1; j < matrix[0].Length; ++j)
+            {
+                if (matrix[0][j] == 0)
+                {
+                    for (int i = 1; i < matrix.Length; ++i)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+
+            if (zeroRow)
+            {
+                for (int j = 0; j < matrix[0].Length; ++j)
+                {
+                    matrix[0][j] = 0;
+                }
+            }
+
+            if(zeroColumn)
+            {
+                for (int i = 0; i < matrix.Length; ++i)
+                {
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+    }
+}
