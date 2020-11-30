8b157c727ce4c51511ab6b9155093b1c2f17d1a9
josephttran <42635012+josephttran@users.noreply.github.com>
2020-11-29 21:09:29 -0800

    Add code for Same Tree

---
+++ Csharp/Code.Tests/TreeTests/IsSameTreeTests.cs
@@ -0,0 +1,52 @@
+﻿using Code.Models;
+using NUnit.Framework;
+using System.Collections;
+
+namespace Code.Tests.TreeTests
+{
+    [TestFixture]
+    class IsSameTreeTests
+    {
+        private Tree _tree;
+
+        [SetUp]
+        public void Setup()
+        {
+            _tree = new Tree();
+        }
+
+        [TestCaseSource(typeof(DataSource), nameof(DataSource.TestCases))]
+        public void IsSameTree_Tests(TreeNode tree1, TreeNode tree2, bool expected)
+        {
+            bool result = _tree.IsSameTree(tree1, tree2);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        private class DataSource
+        {
+            public static IEnumerable TestCases
+            {
+                get
+                {
+                    TreeNode firstP = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+                    TreeNode firstQ = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+                    bool firstExpected = true;
+
+
+                    TreeNode secondP = new TreeNode(1, new TreeNode(2), null);
+                    TreeNode secondQ = new TreeNode(1, null, new TreeNode(2));
+                    bool secondExpected = false;
+
+                    TreeNode thirdP = new TreeNode(1, new TreeNode(2), new TreeNode(1));
+                    TreeNode thirdQ = new TreeNode(1, new TreeNode(1), new TreeNode(2));
+                    bool thirdExpected = false;
+
+                    yield return new TestCaseData(firstP, firstQ, firstExpected);
+                    yield return new TestCaseData(secondP, secondQ, secondExpected);
+                    yield return new TestCaseData(thirdP, thirdQ, thirdExpected);
+                }
+            }
+        }
+    }
+}
--- Csharp/Code/Tree.cs
+++ Csharp/Code/Tree.cs
@@ -7,6 +7,30 @@ namespace Code
 {
     public class Tree
     {
+        /* Given two binary trees, write a function to check if they are the same or not.
+         * Two binary trees are considered the same 
+         * if they are structurally identical and the nodes have the same value.
+         */
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+            {
+                return true;
+            }
+
+            if (p == null || q == null)
+            {
+                return false;
+            }
+
+            if (p.val != q.val)
+            {
+                return false;
+            }
+
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+
         /* Given the root of a binary tree, return its maximum depth.
          * A binary tree's maximum depth is the number of nodes along the longest path 
          * from the root node down to the farthest leaf node.
