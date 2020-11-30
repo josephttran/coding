6d9856086e6361648d6c128869b322dd15431189
josephttran <42635012+josephttran@users.noreply.github.com>
2020-11-29 19:45:58 -0800

    Add code for Maximum Depth of Binary Tree

---
+++ Csharp/Code.Tests/TreeTests/MaxDepthTests.cs
@@ -0,0 +1,69 @@
+﻿using Code.Models;
+using NUnit.Framework;
+using System.Collections;
+
+namespace Code.Tests.TreeTests
+{
+    [TestFixture]
+    class MaxDepthTests
+    {
+        private Tree _tree;
+
+        [SetUp]
+        public void Setup()
+        {
+            _tree = new Tree();
+        }
+
+        [Test]
+        public void MaxDepth_ShouldBeZero_WhenNoNodes()
+        {
+            TreeNode tree = new TreeNode();
+            tree = null;
+            int expected = 0;
+
+            int result = _tree.MaxDepth(tree);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void MaxDepth_ShouldBeOne_WhenSingleNode()
+        {
+            TreeNode tree = new TreeNode(0);
+            int expected = 1;
+
+            int result = _tree.MaxDepth(tree);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCaseSource(typeof(DataSource), nameof(DataSource.TestCases))]
+        public void MaxDepth_ShoudReturnMaxDepth_Tests(TreeNode tree, int expected)
+        {
+            int result = _tree.MaxDepth(tree);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        private class DataSource
+        {
+            public static IEnumerable TestCases
+            {
+                get
+                {
+                    TreeNode first = new TreeNode(1, null, new TreeNode(2));
+                    int expectedFirst = 2;
+
+                    TreeNode second = new TreeNode(1, new TreeNode(9), new TreeNode(20));
+                    second.right.left = new TreeNode(15);
+                    second.right.right = new TreeNode(7);
+                    int expectedSecond = 3;
+
+                    yield return new TestCaseData(first, expectedFirst);
+                    yield return new TestCaseData(second, expectedSecond);
+                }
+            }
+        }
+    }
+}
---
+++ Csharp/Code/Tree.cs
@@ -0,0 +1,28 @@
+﻿using Code.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code
+{
+    public class Tree
+    {
+        /* Given the root of a binary tree, return its maximum depth.
+         * A binary tree's maximum depth is the number of nodes along the longest path 
+         * from the root node down to the farthest leaf node.
+         */
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            int maxLeft = MaxDepth(root.left);
+            int maxRight = MaxDepth(root.right);
+
+            return 1 + Math.Max(maxLeft, maxRight);
+        }
+    }
+}
+
