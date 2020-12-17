58e5073f34804d3eb2d3f7ba0f1192661970d5e3
josephttran <42635012+josephttran@users.noreply.github.com>
2020-12-16 21:00:06 -0800

    Add code for Design Add and Search Words Data Structure

---
+++ Csharp/Code.Tests/WordDictionaryTests/WordDictionaryTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace Code.Tests.WordDictionaryTests
+{
+    [TestFixture]
+    class WordDictionaryTests
+    {
+        WordDictionary _wordDictionary;
+
+        [SetUp]
+        public void Setup()
+        {
+            _wordDictionary = new WordDictionary();
+        }
+
+        [Test]
+        public void WordDictionary_Tests()
+        {
+            _wordDictionary.AddWord("bad");
+            _wordDictionary.AddWord("dad");
+            _wordDictionary.AddWord("mad");
+
+            Assert.Multiple(() =>
+            {
+                Assert.IsFalse(_wordDictionary.Search("pad"));
+                Assert.IsTrue(_wordDictionary.Search("bad"));
+                Assert.IsTrue(_wordDictionary.Search(".ad"));
+                Assert.IsTrue(_wordDictionary.Search("b.."));
+            });
+        }
+    }
+}
---
+++ Csharp/Code/WordDictionary.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code
+{
+    /* Design a data structure that supports adding new words and finding if a string matches any previously added string.
+     */
+    public class WordDictionary
+    {
+        Dictionary<int, List<string>> lengthWords;
+
+        public WordDictionary()
+        {
+            lengthWords = new Dictionary<int, List<string>>();
+        }
+
+        public void AddWord(string word)
+        {
+            int wordLength = word.Length;
+
+            if (lengthWords.TryGetValue(wordLength, out List<string> wordList))
+            {
+                if (!wordList.Contains(word))
+                {
+                    lengthWords[wordLength].Add(word);
+                }
+            }
+            else
+            {
+                List<string> list = new List<string>();
+                list.Add(word);
+                lengthWords.Add(wordLength, list);
+            }
+        }
+
+        public bool Search(string word)
+        {
+            int wordLength = word.Length;
+
+            if (lengthWords.TryGetValue(wordLength, out List<string> wordList))
+            {
+                foreach (string str in wordList)
+                {
+                    bool same = true;
+
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (word[i] == '.' || word[i] == str[i])
+                        {
+                            continue;
+                        }
+
+                        same = false;
+                    }
+
+                    if (same)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
