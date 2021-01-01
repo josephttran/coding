f52f49e6cf17f4be2d3dd644fc077cb9f275b677
josephttran <42635012+josephttran@users.noreply.github.com>
2021-01-01 00:37:41 -0800

    Add code for Merge Intervals

---
+++ Csharp/Code.Tests/IntervalTests/MergeTests.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+
+namespace Code.Tests.IntervalTests
+{
+    [TestFixture]
+    class MergeTests
+    {
+        Interval _interval;
+
+        [SetUp]
+        public void Setup()
+        {
+            _interval = new Interval();
+        }
+
+        [Test]
+        public void Merge_ShouldMergeIntervalWhenOverLap()
+        {
+            int[][] intervals = {
+                new int[] { 1, 3 },
+                new int[] { 8, 10 },
+                new int[] { 2, 6 },
+                new int[] { 15, 18 } };
+
+            int[][] expected = { 
+                new int[] { 1, 6 },
+                new int[] { 8, 10 },
+                new int[] { 15, 18 }};
+
+            int[][] result = _interval.Merge(intervals);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Merge_ShouldMergeIntervalWhenOverLap_Test2()
+        {
+            int[][] intervals = {
+                new int[] { 1, 4 },
+                new int[] { 4, 5 }};
+ 
+            int[][] expected = { new int[] { 1, 5 } };
+
+            int[][] result = _interval.Merge(intervals);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Merge_ShouldMergeIntervalWhenOverLap_Test3()
+        {
+            int[][] intervals = {
+                new int[] { 1, 4 },
+                new int[] { 5, 6 }};
+
+            int[][] expected = { 
+                new int[] { 1, 4 },
+                new int[] { 5, 6 }};
+
+            int[][] result = _interval.Merge(intervals);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Merge_ShouldMergeIntervalWhenOverLap_Test4()
+        {
+            int[][] intervals = {
+                new int[] { 1, 4 },
+                new int[] { 2, 3 }};
+
+            int[][] expected = {
+                new int[] { 1, 4 }};
+
+            int[][] result = _interval.Merge(intervals);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Merge_ShouldMergeIntervalWhenOverLap_Test5()
+        {
+            int[][] intervals = {
+                new int[] { 1, 4 },
+                new int[] { 0, 2 },
+                new int[] { 3, 5 }};
+
+            int[][] expected = {
+                new int[] { 0, 5 }};
+
+            int[][] result = _interval.Merge(intervals);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Merge_ShouldMergeIntervalWhenOverLap_Test6()
+        {
+            int[][] intervals = {
+                new int[] { 2, 3 },
+                new int[] { 2, 2 },
+                new int[] { 3, 3 },
+                new int[] { 1, 3 },
+                new int[] { 5, 7 },
+                new int[] { 2, 2 },
+                new int[] { 4, 6 }};
+
+            int[][] expected = {
+                new int[] { 1, 3 },
+                new int[] { 4, 7 }};
+
+            int[][] result = _interval.Merge(intervals);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Csharp/Code/Interval.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Code
+{
+    public class Interval
+    {
+        /* Given an array of intervals where intervals[i] = [starti, endi], 
+         * merge all overlapping intervals, and 
+         * return an array of the non-overlapping intervals that cover all the intervals in the input.
+         */
+        public int[][] Merge(int[][] intervals)
+        {
+            if (intervals.Length < 2)
+            {
+                return intervals;
+            }
+
+            Array.Sort(intervals, (a, b) =>
+            {
+                return a[0].CompareTo(b[0]);
+            });
+
+            List<int[]> list = new List<int[]>();
+
+            list.Add(intervals[0]);
+
+            int index = 0;
+
+            for (int i = 1; i < intervals.Length; i++)
+            {
+                if (list[index][1] < intervals[i][0])
+                {
+                    index++;
+                    list.Add(intervals[i]);
+                }
+                else
+                {
+                    list[index][1] = Math.Max(list[index][1], intervals[i][1]);
+                }
+            }
+
+            return list.ToArray();
+        }
+    }
+}
