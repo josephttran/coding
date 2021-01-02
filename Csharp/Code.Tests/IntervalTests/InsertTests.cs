d7f485624bc427ee9bb5766250b3b17718d5c0e2
josephttran <42635012+josephttran@users.noreply.github.com>
2021-01-01 20:09:03 -0800

    Add code for Insert Interval

---
+++ Csharp/Code.Tests/IntervalTests/InsertTests.cs
@@ -0,0 +1,99 @@
+﻿using NUnit.Framework;
+using System.Collections;
+
+namespace Code.Tests.IntervalTests
+{
+    [TestFixture]
+    class InsertTests
+    {
+        Interval _interval;
+
+        [SetUp]
+        public void Setup()
+        {
+            _interval = new Interval();
+        }
+
+        [Test]
+        public void Insert_ShouldContainOnlyNewInterval_WhenIntervalsIsEmpty()
+        {
+            int[][] intervals = { };
+            int[] newInterval = { 1, 2 };
+            int[][] expected = 
+            { 
+                new int[] { 1, 2 } 
+            };
+
+            int[][] result = _interval.Insert(intervals, newInterval);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestCaseSource(typeof(DataSource), nameof(DataSource.TestCases))]
+        public void Insert_ShouldInsertNewIntervalOrMerge(int[][] intervals, int[] newInterval, int[][] expected)
+        {
+            int[][] result = _interval.Insert(intervals, newInterval);
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        class DataSource
+        {
+            public static IEnumerable TestCases
+            {
+                get
+                {
+                    // First Test Data
+                    int[][] firstIntervals = { 
+                        new int[] { 1, 3 }, 
+                        new int[] { 6, 9 } };
+                    int[] firstNewInterval = { 2, 5 };
+                    int[][] firstOutput = { 
+                        new int[] { 1, 5 },
+                        new int[] { 6, 9 } };
+
+                    // Second Test Data
+                    int[][] secondIntervals = { new int[] { 1, 5 } };
+                    int[] secondNewInterval = { 2, 3 };
+                    int[][] secondOutput = { new int[] { 1, 5 } };
+
+                    // Third Test Data
+                    int[][] thirdIntervals = { new int[] { 1, 5 } };
+                    int[] thirdNewInterval = { 2, 7 };
+                    int[][] thirdOutput = { new int[] { 1, 7 } };
+                    
+                    // Fourth Test Data
+                    int[][] fourthIntervals = {
+                        new int[] { 1, 2 },
+                        new int[] { 3, 5 },
+                        new int[] { 6, 7 },
+                        new int[] { 8, 10 },
+                        new int[] { 12, 16 }};
+                    int[] fourthNewInterval = { 4, 8 };
+                    int[][] fourthOutput = {
+                        new int[] { 1, 2 },
+                        new int[] { 3, 10 },
+                        new int[] { 12, 16 }};
+
+                    // Fifth Test Data
+                    int[][] fifthIntervals = {
+                        new int[] { 2, 5 },
+                        new int[] { 6, 7 },
+                        new int[] { 8, 9 }};
+                    int[] fifthNewInterval = { 0, 1 };
+                    int[][] fifthOutput = {
+                        new int[] { 0, 1 },
+                        new int[] { 2, 5 },
+                        new int[] { 6, 7 },
+                        new int[] { 8, 9 }};
+
+                    yield return new TestCaseData(firstIntervals, firstNewInterval, firstOutput);
+                    yield return new TestCaseData(secondIntervals, secondNewInterval, secondOutput);
+                    yield return new TestCaseData(thirdIntervals, thirdNewInterval, thirdOutput);
+                    yield return new TestCaseData(fourthIntervals, fourthNewInterval, fourthOutput);
+                    yield return new TestCaseData(fifthIntervals, fifthNewInterval, fifthOutput);
+                }
+            }
+        }
+    }
+}
--- Csharp/Code/Interval.cs
+++ Csharp/Code/Interval.cs
@@ -6,6 +6,43 @@ namespace Code
 {
     public class Interval
     {
+        /* Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+         * You may assume that the intervals were initially sorted according to their start times.
+         */
+        public int[][] Insert(int[][] intervals, int[] newInterval)
+        {
+            List<int[]> list = new List<int[]>();
+            int newIntervalStart = newInterval[0];
+            int newIntervalEnd = newInterval[1];
+
+            for (int i = 0; i < intervals.Length; i++)
+            {
+                if (intervals[i][1] < newIntervalStart)
+                {
+                    list.Add(intervals[i]);
+                }
+                else if (intervals[i][0] > newIntervalEnd)
+                {
+                    list.Add(new int[] { newIntervalStart, newIntervalEnd });
+                    newIntervalStart = int.MaxValue;
+                    newIntervalEnd = int.MaxValue;
+                    list.Add(intervals[i]);
+                }
+                else
+                {
+                    newIntervalStart = Math.Min(newIntervalStart, intervals[i][0]);
+                    newIntervalEnd = Math.Max(newIntervalEnd, intervals[i][1]);
+                }
+            }
+
+            if (newIntervalStart != int.MaxValue)
+            {
+                list.Add(new int[] { newIntervalStart, newIntervalEnd });
+            }
+
+            return list.ToArray();
+        }
+
         /* Given an array of intervals where intervals[i] = [starti, endi], 
          * merge all overlapping intervals, and 
          * return an array of the non-overlapping intervals that cover all the intervals in the input.
