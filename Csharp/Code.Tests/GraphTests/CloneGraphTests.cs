e25885ff53ce89a505f7b8db427c56d6138323fa
josephttran <42635012+josephttran@users.noreply.github.com>
2021-01-09 21:22:52 -0800

    Add code for Clone Graph

---
+++ Csharp/Code.Tests/GraphTests/CloneGraphTests.cs
@@ -0,0 +1,131 @@
+﻿using Code.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Code.Tests.GraphTests
+{
+    [TestFixture]
+    class CloneGraphTests
+    {
+        private Graph _graph;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _graph = new Graph();
+        }
+
+        [Test]
+        public void CloneGraph_ShouldBeEmpty()
+        {
+            Node node = null;
+            Node expected = null;
+
+            Node result = _graph.CloneGraph(node);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void CloneGraph_ShouldBeOne()
+        {
+            Node node = new Node(1);
+            Node expected = new Node(1);
+            int expectedNeighbor = 0;
+
+            Node result = _graph.CloneGraph(node);
+
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expected.val, result.val);
+                Assert.AreEqual(expectedNeighbor, expected.neighbors.Count);
+                Assert.AreEqual(expectedNeighbor, result.neighbors.Count);
+            });
+        }
+
+        [Test]
+        public void CloneGraph_ShouldDeepCloneOne()
+        {
+            Node node = new Node(1);
+            Node expected = new Node(1);
+            int expectedNeighbor = 0;
+
+            Node result = _graph.CloneGraph(node);
+
+            Assert.Multiple(() => 
+            {
+                Assert.IsFalse(result.Equals(node));
+                Assert.AreEqual(expected.val, result.val);
+                Assert.AreEqual(expectedNeighbor, expected.neighbors.Count);
+                Assert.AreEqual(expectedNeighbor, result.neighbors.Count);
+            });
+        }
+
+        [Test]
+        public void CloneGraph_ShouldDeepCopyGraph()
+        {
+            Node one = new Node(1);
+            Node two = new Node(2);
+            Node three = new Node(3);
+            Node four = new Node(4);
+
+            one.neighbors = new List<Node> { two, four };
+            two.neighbors = new List<Node> { one, three };
+            three.neighbors = new List<Node> { two, four };
+            four.neighbors = new List<Node> { one, three };
+
+            int expectedOneVal = 1;
+            int expectedTwoVal = 2;
+            int expectedThreeVal = 3;
+            int expectedFourVal = 4;
+            int[] expectedOneNeighbors = { 2, 4 };
+            int[] expectedTwoNeighbors = { 1, 3 };
+            int[] expectedThreeNeighbors = { 2, 4 };
+            int[] expectedFourNeighbors = { 1, 3 };
+
+            Node result = _graph.CloneGraph(one);
+
+            Assert.Multiple(() =>
+            {
+                Node currNode = result;
+                Assert.IsFalse(currNode.Equals(one));
+                Assert.AreEqual(expectedOneVal, currNode.val);
+                Assert.IsTrue(IsSameNeighbors(expectedOneNeighbors, currNode.neighbors));
+
+                currNode = currNode.neighbors[0];
+                Assert.IsFalse(currNode.Equals(two));
+                Assert.AreEqual(expectedTwoVal, currNode.val);
+                Assert.IsTrue(IsSameNeighbors(expectedTwoNeighbors, currNode.neighbors));
+
+                currNode = currNode.neighbors[1];
+                Assert.IsFalse(currNode.Equals(three));
+                Assert.AreEqual(expectedThreeVal, currNode.val);
+                Assert.IsTrue(IsSameNeighbors(expectedThreeNeighbors, currNode.neighbors));
+
+                currNode = currNode.neighbors[1];
+                Assert.IsFalse(currNode.Equals(four));
+                Assert.AreEqual(expectedFourVal, currNode.val);
+                Assert.IsTrue(IsSameNeighbors(expectedFourNeighbors, currNode.neighbors));
+            });
+        }
+
+        private bool IsSameNeighbors(int[] expected, IList<Node> neighbors)
+        {
+            if (expected.Length != neighbors.Count)
+            {
+                return false;
+            }
+
+            foreach (Node node in neighbors)
+            {
+                if (Array.IndexOf(expected, node.val) == -1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Csharp/Code/Graph.cs
@@ -0,0 +1,63 @@
+﻿using Code.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Code
+{
+    public class Graph
+    {
+        /* Given a reference of a node in a connected undirected graph.
+         * Return a deep copy (clone) of the graph.
+         * Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
+         * 
+         * There is no repeated edges and no self-loops in the graph.
+         */
+        public Node CloneGraph(Node node)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            HashSet<Node> visited = new HashSet<Node>();
+            Queue<Node> q = new Queue<Node>();
+            q.Enqueue(node);
+
+            List<Node> cloneNode = new List<Node>();
+
+            while (q.Count > 0)
+            {
+                Node currNode = q.Dequeue();
+
+                if (!visited.Contains(currNode))
+                {
+                    visited.Add(currNode);
+                    cloneNode.Add(new Node(currNode.val));
+                }
+
+                foreach (Node neighbor in currNode.neighbors)
+                {
+                    if (!visited.Contains(neighbor))
+                    {
+                        q.Enqueue(neighbor);
+                    }
+                }
+            }
+
+            cloneNode.Sort((a, b) => a.val.CompareTo(b.val));
+
+            foreach (Node currNode in visited)
+            {
+                cloneNode[currNode.val - 1].neighbors = new List<Node>();
+
+                foreach (Node neighbor in currNode.neighbors)
+                {
+                    cloneNode[currNode.val - 1].neighbors.Add(cloneNode[neighbor.val - 1]);
+                }
+            }
+
+            return cloneNode[0];
+        }
+    }
+}
