79a01390355a491fae9070c68f10bcd7726f1b9c
josephttran <42635012+josephttran@users.noreply.github.com>
2021-01-29 23:38:07 -0800

    Remove

--- Csharp/Code.Tests/GraphTests/PacificAtlanticTests.cs
+++ Csharp/Code.Tests/GraphTests/PacificAtlanticTests.cs
@@ -17,14 +17,6 @@ namespace Code.Tests.GraphTests
         [Test]
         public void PacificAtlantic_TestWhenEmpty()
         {
-            /*  Pacific ~   ~   ~   ~   ~ 
-                   ~  1   2   2   3  (5) *
-                   ~  3   2   3  (4) (4) *
-                   ~  2   4  (5)  3   1  *
-                   ~ (6) (7)  1   4   5  *
-                   ~ (5)  1   1   2   4  *
-                      *   *   *   *   * Atlantic
-             */
             int[][] matrix = null;
 
             IList<IList<int>> expected = new List<IList<int>>();
